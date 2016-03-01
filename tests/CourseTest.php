5b96035fc10b1ea387ccd40c6857aca892844206
Afton Downey and Mollie Bootsma <aaike001@gmail.com>
2016-03-01 09:12:49 -0800

    pass deleteAll test for Course

--- tests/CourseTest.php
+++ tests/CourseTest.php
@@ -36,6 +36,49 @@
             //Assert
             $this->assertEquals($test_course, $result[0]);
         }
+
+        function testGetAll()
+        {
+            //Arrange
+            $name = "Econ";
+            $course_number = 200;
+            $id = 2;
+            $name2 = "Physics";
+            $course_number2 = 401;
+            $id2 = 3;
+            $test_course = new Course($name, $course_number, $id);
+            $test_course->save();
+            $test_course2 = new Course($name2, $course_number2, $id2);
+            $test_course2->save();
+
+            //Act
+            $result = Course::getAll();
+
+            //Assert
+            $this->assertEquals([$test_course, $test_course2], $result);
+        }
+
+        function testDelete()
+        {
+            //Arrange
+            $name = "Econ";
+            $course_number = 200;
+            $id = 2;
+            $name2 = "Physics";
+            $course_number2 = 401;
+            $id2 = 3;
+            $test_course = new Course($name, $course_number, $id);
+            $test_course->save();
+            $test_course2 = new Course($name2, $course_number2, $id2);
+            $test_course2->save();
+
+            //Act
+            Course::deleteAll();
+
+            //Assert
+            $result = Course::getAll();
+            $this->assertEquals([], $result);
+        }
     }
 
 
