ad8df874e93aa5da95e41f2e49c929e881d5a6e8
Afton Downey and Mollie Bootsma <student@epicodus.com>
2016-03-01 09:01:51 -0800

    test save function passing

---
+++ tests/CourseTest.php
@@ -0,0 +1,57 @@
+<?php
+
+    /**
+    * @backupGlobals disabled
+    * @backupStaticAttributes disabled
+    */
+
+    require_once "src/Course.php";
+    require_once "src/Student.php";
+
+    $server = 'mysql:host=localhost;dbname=registrar_test';
+    $username = 'root';
+    $password = 'root';
+    $DB = new PDO($server, $username, $password);
+
+    class CourseTest extends PHPUnit_Framework_TestCase
+    {
+        protected function tearDown()
+        {
+            Course::deleteAll();
+            //Student::deleteAll();
+        }
+
+        function testSave()
+        {
+            //Arrange
+            $name = "History";
+            $course_number = 101;
+            $id = 1;
+            $test_course = new Course($name, $course_number, $id);
+            $test_course->save();
+
+            //Act
+            $result = Course::getAll();
+
+            //Assert
+            $this->assertEquals($test_course, $result[0]);
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
